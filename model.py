3e5711682ee6fcf4eec39a93a994674f2a54c1aa
cedoobadoo <cedar.r.garcia@gmail.com>
2019-06-04 14:36:22 -0500

    created model for classification

---
+++ model.py
@@ -0,0 +1,29 @@
+from keras.models import Sequential
+from keras.layers import (Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense)
+
+
+def model(num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(156, 156, 3))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    return model
+
